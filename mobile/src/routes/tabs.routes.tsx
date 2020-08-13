bb5f015e4f5c6babaaf7fae7f78940f18f2b8171
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-08-13 02:57:12 -0300

    all tabs routes done

--- mobile/src/routes/tabs.routes.tsx
+++ mobile/src/routes/tabs.routes.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Ionicons } from '@expo/vector-icons'
 
 import Proffys from '../views/Proffys'
 import Favorites from '../views/Favorites'
@@ -8,9 +9,22 @@ const Tabs = createBottomTabNavigator()
 
 const TabsRoutes = () => {
   return (
-    <Tabs.Navigator>
-      <Tabs.Screen name='Proffys' component={Proffys} />
-      <Tabs.Screen name='Favorites' component={Favorites} />
+    <Tabs.Navigator tabBarOptions={{
+      activeBackgroundColor: '#7159ac',
+      activeTintColor: '#fff',
+      inactiveTintColor: '#000',
+      labelStyle: { fontSize: 16, fontWeight: "bold" }
+    }}>
+      <Tabs.Screen options={{
+        tabBarIcon: ({ color, size, focused }) => {
+          return <Ionicons name='ios-easel' size={size} color={focused ? '#fff' : '#000'} />
+        }
+      }} name='Proffys' component={Proffys} />
+      <Tabs.Screen options={{
+        tabBarIcon: ({ color, size, focused }) => {
+          return <Ionicons name='ios-heart' size={size} color={focused ? '#fff' : '#000'} />
+        }
+      }} name='Favorites' component={Favorites} />
     </Tabs.Navigator>
   )
 }
